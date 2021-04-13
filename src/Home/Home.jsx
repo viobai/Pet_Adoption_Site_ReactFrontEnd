b53963ce3d03aebda88c56875a3ea55fc097b142
Viola Bai <bai.tian@northeastern.edu>
2021-04-12 21:55:43 -0700

    home css done 1/2

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,15 +5,18 @@ import Pets from '@material-ui/icons/Pets';
 
 const Home = () => {
     return (
-        <div className={style.home}>
-            <img src="images/dog-family.png" alt="New Adopted Pet"/>
-            <h1>Rescue Project: <br/>Humane</h1>
-            <div className={style.homeDesc}>
-                <p>Each year, it's estimated that more than one million adoptable dogs and cats are euthanized in the United States.</p>
-                <p>When you adopt, you save a loving animal by making them part of your family and open up shelter space for another animal who might desperately need it.</p>
-                <p>You'll change a homeless animal's whole world and get a new best friend. Seriously, what could be better than that?</p>
+        <div className={style.homeContainer}>
+            <div className={style.homeTop}>
+                <h1>Rescue Project: <br/>Humane</h1>
+                <div className={style.homeDesc}>
+                    <p>When you adopt, you'll change a homeless animal's whole world and get a new best friend.</p>
+                    <p>Seriously, what could be better than that?</p>
+                </div>
+                <Link className={style.startBtn} to="/petlist"><span>Get started</span> <span className={style.petIcon}><Pets/></span></Link>
             </div>
-            <Link className={style.startBtn} to="/petlist"><span>Get started</span> <span className={style.petIcon}><Pets/></span></Link>
+            <div className={style.homeBottom}>
+                <img alt="New Pet and Family" src="https://github.com/viobai/seainfo6150-final-project-webapp/blob/master/src/images/dog-family.png?raw=true" alt="New Adopted Pet"/>
+            </div>
         </div>
     )
 }
