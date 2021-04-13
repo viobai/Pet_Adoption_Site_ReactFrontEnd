0bd90ba6b491c28710a6f76e3adf8f4e463b5a07
Viola Bai <bai.tian@northeastern.edu>
2021-04-13 11:33:30 -0700

    added img

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -7,23 +7,29 @@ import Pets from '@material-ui/icons/Pets';
 
 const Home = (props) => {
     var petsDisplay = [];
-    for (var i = 0; i < 3; i++) {
-        petsDisplay[i] = props.pets[~~(Math.random() * props.pets.length)];
+    var randomIdx = [];
+    while(randomIdx.length < 3){
+        var r = ~~(Math.random() * props.pets.length);
+        if(randomIdx.indexOf(r) === -1) {
+            randomIdx.push(r);
+            petsDisplay.push(props.pets[r]);
+        }
     }
     
 
     return (
         <div className={style.homeContainer}>
             <div className={style.homeTop}>
-                <h1>Rescue Project: <br/>Humane</h1>
                 <div className={style.homeDesc}>
                     <p>When you adopt, you'll change a homeless animal's whole world and get a new best friend.</p>
                     <p>Seriously, what could be better than that?</p>
                 </div>
+                <h1>Rescue Project: <br/>Humane</h1>
                 <Link className={style.startBtn} to="/petlist"><span>Get started</span> <span className={style.petIcon}><Pets/></span></Link>
             </div>
             <div className={style.homeBottom}>
                 <img alt="New Pet and Family" src="https://github.com/viobai/seainfo6150-final-project-webapp/blob/master/src/images/dog-family.png?raw=true" alt="New Adopted Pet"/>
+                <h3>Some of our available pets ...</h3>
                 <div className={style.randomPetDisplay} id="randomPetDisplay">
                     <PetListItem pet={petsDisplay[0]}/>
                     <PetListItem pet={petsDisplay[1]}/>
